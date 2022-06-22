03c4dc141d628b14d09504819291e0c8824fe784
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-06-22 20:29:25 -0300

    basic pokemon list design

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  pokemons = [
+    {
+      name: 'Bulbassaur',
+      sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/1.png',
+      id: 1,
+    },
+    {
+      name: 'Ivyssaur',
+      sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/2.png',
+      id: 2,
+    },
+    {
+      name: 'Venussaur',
+      sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/3.png',
+      id: 3,
+    },
+    {
+      name: 'Charmander',
+      sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/4.png',
+      id: 4,
+    },
+    {
+      name: 'Charmeleon',
+      sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/5.png',
+      id: 5,
+    },
+    {
+      name: 'Charizard',
+      sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/6.png',
+      id: 6,
+    },
+  ];
+
   constructor() {}
 
 }
